85122b70b493d3b2f7ab100fd0904ec31de2ac50
JBraddockm <jbraddockm@gmail.com>
2024-03-10 13:42:13 +0000

    Created a 'ThemeSwitcher' components to enable toggling between dark and light mode in Tailwind CSS

---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import useDarkMode from 'src/hooks/useDarkMode';
+
+export default function ThemeSwitcher() {
+  const [colorTheme, setTheme] = useDarkMode();
+  const [darkMode, setDarkMode] = useState(colorTheme === 'light');
+
+  const toggleDarkMode = (checked) => {
+    setTheme(colorTheme);
+    setDarkMode(checked);
+  };
+
+  return (
+    <>
+      <button className="rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+        <DarkModeSwitch checked={darkMode} size={20} onChange={toggleDarkMode} />
+      </button>
+    </>
+  );
+}
