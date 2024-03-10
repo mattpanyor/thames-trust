a05dd774b7d2a5e459e50c0a0c4f2eb19443b3fe
JBraddockm <jbraddockm@gmail.com>
2024-03-10 13:51:26 +0000

    Added a test for App.jsx

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import App from 'src/App';
+
+describe('<App/> Component', () => {
+  it('Renders Without Error', () => {
+    render(<App />);
+  });
+});
