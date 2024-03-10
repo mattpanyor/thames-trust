637bbb628cf3bc9881236362032fd0c22f383234
JBraddockm <jbraddockm@gmail.com>
2024-03-10 13:51:11 +0000

    Added a sample view in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import ThemeSwitcher from 'src/components/ThemeSwitcher.jsx';
+
 function App() {
   return (
     <>
+      <main className="bg-gray-50 dark:bg-gray-900">
+        <div className="mx-auto flex h-screen flex-col items-center justify-center px-6 xl:px-0 dark:bg-gray-900">
+          <div className="text-center xl:max-w-4xl">
+            <h1 className="mb-3 text-2xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl dark:text-white">
+              Vite React
+            </h1>
+            <ThemeSwitcher />
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
