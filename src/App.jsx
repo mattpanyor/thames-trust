808aa0eb06d10b643be15698c262f5112ef65373
JBraddockm <jbraddockm@gmail.com>
2024-03-10 21:16:03 +0000

    Created app's folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <>
       <main className="bg-gray-50 dark:bg-gray-900">
-        <div className="mx-auto flex h-screen flex-col items-center justify-center px-6 xl:px-0 dark:bg-gray-900">
+        <div className="mx-auto flex h-screen flex-col items-center justify-center px-6 dark:bg-gray-900 xl:px-0">
           <div className="text-center xl:max-w-4xl">
-            <h1 className="mb-3 text-2xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl dark:text-white">
+            <h1 className="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl">
               Vite React
             </h1>
             <ThemeSwitcher />
