34a57d0b9f6a0550ded473a72aada350dcf96890
JBraddockm <jbraddockm@gmail.com>
2024-03-11 14:06:38 +0000

    Refactored App.jsx to render NavbarSidebarLayout by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import ThemeSwitcher from 'src/components/ThemeSwitcher.jsx';
+import NavbarSidebarLayout from 'src/layouts/NavbarSidebarLayout.jsx';
 
 function App() {
   return (
     <>
-      <main className="bg-gray-50 dark:bg-gray-900">
-        <div className="mx-auto flex h-screen flex-col items-center justify-center px-6 dark:bg-gray-900 xl:px-0">
-          <div className="text-center xl:max-w-4xl">
-            <h1 className="mb-3 text-2xl font-bold leading-tight text-gray-900 dark:text-white sm:text-4xl lg:text-5xl">
-              Vite React
-            </h1>
-            <ThemeSwitcher />
-          </div>
+      <NavbarSidebarLayout>
+        <div className="mb-6 flex flex-wrap items-center justify-between">
+          <h2 className="ml-3 text-lg font-semibold dark:text-gray-400">Dashboard Main Content</h2>
         </div>
-      </main>
+      </NavbarSidebarLayout>
     </>
   );
 }
