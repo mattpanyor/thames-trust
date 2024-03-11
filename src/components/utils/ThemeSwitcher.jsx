881ae2cac21dd8069fbc08faf39b560695000901
JBraddockm <jbraddockm@gmail.com>
2024-03-11 13:52:10 +0000

    Exported utils components to index.js

--- src/components/utils/ThemeSwitcher.jsx
+++ src/components/utils/ThemeSwitcher.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
-import useDarkMode from 'src/hooks/useDarkMode';
+import { useDarkMode } from 'src/hooks/index.js';
 
-export default function ThemeSwitcher() {
+export function ThemeSwitcher() {
   const [colorTheme, setTheme] = useDarkMode();
   const [darkMode, setDarkMode] = useState(colorTheme === 'light');
 
