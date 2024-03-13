ca9e1b222e2c1a277b0298c66d6e20f3ec83d566
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-03-13 19:53:57 +0000

    Network latency simulation function added

---
+++ src/components/utils/NetworkLatency.jsx
@@ -0,0 +1,8 @@
+// function to simulate network latency 
+import React from "react";
+
+const networkSimulator = () => {
+    // This is defining a state variable 'loading' and a function 'setLoading' to update it
+    const [loading, setLoading] = useState(true);
+
+}
