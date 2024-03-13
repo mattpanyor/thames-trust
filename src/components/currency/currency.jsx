8b444ef5586573bb528952e6fe869aa973edb434
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-03-13 18:54:41 +0000

    currency converted to gbp

--- src/components/currency/currency.jsx
+++ src/components/currency/currency.jsx
@@ -4,7 +4,7 @@ function CurrencyDisplay(props) {
   const { amount } = props;
 
   //this is the format for the currency 
-  const formattedCurrency = amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  const formattedCurrency = amount.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' });
   return <span>{formattedCurrency}</span>;}
   export default CurrencyDisplay;
 
