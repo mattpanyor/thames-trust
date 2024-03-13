e504445434c80cdd0f9c8e39e1657ab8151a5188
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-03-13 19:58:59 +0000

    time delay added

--- src/components/utils/NetworkLatency.jsx
+++ src/components/utils/NetworkLatency.jsx
@@ -4,5 +4,12 @@ import React from "react";
 const networkSimulator = () => {
     // This is defining a state variable 'loading' and a function 'setLoading' to update it
     const [loading, setLoading] = useState(true);
+    // useEffect used to run side effects in function component
+  useEffect(() => {
+     // Setting a timeout to change the 'loading' state after 2 seconds
+     const delay = setTimeout (() => {
+      setLoading (true)
+    }, 2000); // Simulating a 2-second delay
+     } )
 
 }
