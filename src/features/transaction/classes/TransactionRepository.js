4e2c962f89bb91e9fefc22a3e8e5b1d1f63510a0
JBraddockm <jbraddockm@gmail.com>
2024-03-14 23:35:28 +0000

    Created findAllTransactionsByUser method.

--- src/features/transaction/classes/TransactionRepository.js
+++ src/features/transaction/classes/TransactionRepository.js
@@ -4,6 +4,17 @@ class TransactionRepository extends EntityPersistence {
   constructor(storageKey) {
     super(storageKey);
   }
+
+  findAllTransactionsByUser(userId) {
+    try {
+      const allTransactions = super.findAll();
+
+      return allTransactions.filter((transaction) => transaction.userId === userId);
+    } catch (error) {
+      console.error('Error retrieving transactions from localStorage:', error);
+      return false;
+    }
+  }
 }
 
 export default TransactionRepository;
