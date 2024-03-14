d1bdcef246886474ee136c3563b98b7ba21148e2
prangchira <prangchira@gmail.com>
2024-03-14 20:22:40 +0000

    Make button a dynamic element

---
+++ src/components/elements/Button.jsx
@@ -0,0 +1,16 @@
+import cn from 'src/utils/twMerge.js';
+
+export function Button({ btnType, btnText, className }) {
+  return (
+    <>
+      <button
+        type={btnType}
+        className={cn(
+          `w-full rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto`,
+          className
+        )}>
+        {btnText}
+      </button>
+    </>
+  );
+}
