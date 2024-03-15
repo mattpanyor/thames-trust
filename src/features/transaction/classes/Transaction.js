4c1d9c37e57fc87247d8146d8875a094d20612d1
JBraddockm <jbraddockm@gmail.com>
2024-03-15 20:43:42 +0000

    Renamed class properties.

--- src/features/transaction/classes/Transaction.js
+++ src/features/transaction/classes/Transaction.js
@@ -1,12 +1,12 @@
 class Transaction {
-    constructor(transaction_id, sender_id, reciever_id, reference, amount, transaction_date) {
-        this.transaction_id = transaction_id;
-        this.sender_id = sender_id;
-        this.reciever_id = reciever_id;
-        this.reference = reference;
-        this.amount = amount;
-        this.transaction_date = transaction_date;
-    }
+  constructor(transactionId, senderId, receiverId, reference, amount, transactionDate) {
+    this.transactionId = transactionId;
+    this.senderId = senderId;
+    this.receiverId = receiverId;
+    this.reference = reference;
+    this.amount = amount;
+    this.transactionDate = transactionDate;
+  }
 }
 
 export default Transaction;
