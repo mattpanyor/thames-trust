c31e75ff474ac82a9d857938639490e1e3ceb7b3
prangchira <prangchira@gmail.com>
2024-03-15 19:47:25 +0000

    add handleonChange to capture login data

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -4,18 +4,16 @@ import { LoginForm } from 'src/features/user/components/login/LoginForm';
 export function Login() {
   return (
     <>
-      <>
-        <NavbarFullWidthLayout>
-          <div className="pt:mt-0 mx-auto flex flex-col items-center justify-center px-6 pt-8 dark:bg-gray-900 md:h-screen">
-            <div className="w-full max-w-xl space-y-8 rounded-lg bg-white p-6 shadow dark:bg-gray-800 sm:p-8">
-              <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                Log in to Online Banking
-              </h2>
-              <LoginForm />
-            </div>
+      <NavbarFullWidthLayout>
+        <div className="pt:mt-0 mx-auto flex flex-col items-center justify-center px-6 pt-8 dark:bg-gray-900 md:h-screen">
+          <div className="w-full max-w-xl space-y-8 rounded-lg bg-white p-6 shadow dark:bg-gray-800 sm:p-8">
+            <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+              Log in to Online Banking
+            </h2>
+            <LoginForm />
           </div>
-        </NavbarFullWidthLayout>
-      </>
+        </div>
+      </NavbarFullWidthLayout>
     </>
   );
 }
