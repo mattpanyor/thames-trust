b82c4ff0f72b95a5862e1a6b45fc3d57481a6f5b
JBraddockm <jbraddockm@gmail.com>
2024-03-15 20:45:06 +0000

    Added conditional styling with twMerge.
    Fixed useEffect dependency.

--- src/features/transaction/components/TransactionListControls.jsx
+++ src/features/transaction/components/TransactionListControls.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react';
+import cn from 'src/utils/twMerge.js';
 
-function TransactionListControls({ pageChange, currentPage, max_length }) {
-    const [btn_next, setBtn_next] = useState(true)
-    const [btn_prev, setBtn_prev] = useState(false)
+export default function TransactionListControls({ pageChange, currentPage, maxLength }) {
+  const [btn_next, setBtn_next] = useState(true);
+  const [btn_prev, setBtn_prev] = useState(false);
 
-    useEffect(() => {
-        if (currentPage <= 0) {
-            setBtn_prev(false)
-        } else {
-            setBtn_prev(true)
-        }
-        if (currentPage >= max_length - 1) {
-            setBtn_next(false)
-        } else {
-            setBtn_next(true)
-        }
-    }, [currentPage])
+  useEffect(() => {
+    if (currentPage <= 0) {
+      setBtn_prev(false);
+    } else {
+      setBtn_prev(true);
+    }
+    if (currentPage >= maxLength - 1) {
+      setBtn_next(false);
+    } else {
+      setBtn_next(true);
+    }
+  }, [currentPage, maxLength]);
 
-    return (
-        <div className="flex items-center justify-around pt-3 sm:pt-6">
-            <button
-                onClick={() => pageChange(currentPage - 1)}
-                disabled={!btn_prev}
-                className='bg-blue-500 text-[1rem] text-white font-bold py-1 px-3 rounded disabled:bg-slate-500'
-            >
-                Prev
-            </button>
-            <button
-                onClick={() => pageChange(currentPage + 1)}
-                disabled={!btn_next}
-                className='bg-blue-500 text-[1rem] text-white font-bold py-1 px-3 rounded disabled:bg-slate-500'
-            >
-                Next
-            </button>
-        </div>
-    )
+  return (
+    <div className="flex items-center space-x-3">
+      <button
+        onClick={() => pageChange(currentPage - 1)}
+        disabled={!btn_prev}
+        className={cn(
+          `dark:focus:ring-blue-800, inline-flex flex-1 items-center justify-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 ${!btn_prev && `cursor-not-allowed bg-blue-400 text-white dark:bg-blue-500`} `
+        )}>
+        Prev
+      </button>
+      <button
+        onClick={() => pageChange(currentPage + 1)}
+        disabled={!btn_next}
+        className={cn(
+          `dark:focus:ring-blue-800, inline-flex flex-1 items-center justify-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 ${!btn_next && `cursor-not-allowed bg-blue-400 text-white dark:bg-blue-500`} `
+        )}>
+        Next
+      </button>
+    </div>
+  );
 }
-
-export default TransactionListControls
