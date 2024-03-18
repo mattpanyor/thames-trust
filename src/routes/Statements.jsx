3ac4c41349afe9e9862f873a6f7b3f41f7aeeded
JBraddockm <jbraddockm@gmail.com>
2024-03-18 01:39:10 +0000

    Changed heading style.

--- src/routes/Statements.jsx
+++ src/routes/Statements.jsx
@@ -5,7 +5,9 @@ export function Statements() {
     <>
       <NavbarSidebarLayout>
         <div className="mb-6 flex flex-wrap items-center justify-between">
-          <h2 className="ml-3 text-lg font-semibold dark:text-gray-400">Statements</h2>
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            Statements
+          </h2>
         </div>
       </NavbarSidebarLayout>
     </>
