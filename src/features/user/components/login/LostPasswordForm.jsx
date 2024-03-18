7cfa28b7a31b258f55ec6be8575d571a52670e99
prangchira <prangchira@gmail.com>
2024-03-18 02:05:32 +0000

    remove heading

--- src/features/user/components/login/LostPasswordForm.jsx
+++ src/features/user/components/login/LostPasswordForm.jsx
@@ -4,9 +4,6 @@ export function LostPasswordForm() {
   return (
     <div className="w-full rounded-lg bg-white shadow dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
       <div className="w-full p-6 sm:p-8">
-        {/* <h2 className="mb-3 text-2xl font-bold text-gray-900 dark:text-white">
-          Forgot your password?
-        </h2> */}
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">
           Please enter your email and we will send you a link to reset your password
         </p>
