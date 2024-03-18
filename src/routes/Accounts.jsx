df089d762810ec58d65dc4b2dca006c43ddcb2a8
JBraddockm <jbraddockm@gmail.com>
2024-03-18 01:39:35 +0000

    Imported new Account components.
    Changed heading style.

--- src/routes/Accounts.jsx
+++ src/routes/Accounts.jsx
@@ -1,18 +1,17 @@
+import { AccountList } from 'src/features/account/components';
 import { NavbarSidebarLayout } from 'src/layouts';
-import AccountObjects from '../features/account/AccountObjects'
 
 export function Accounts() {
   return (
     <>
       <NavbarSidebarLayout>
         <div className="mb-6 flex flex-wrap items-center justify-between">
-          <h2 className="ml-3 text-lg font-semibold dark:text-gray-400">Accounts</h2>
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            Accounts
+          </h2>
         </div>
-        <div>       
-           <AccountObjects/> 
-</div>
+        <AccountList />
       </NavbarSidebarLayout>
     </>
   );
 }
-
