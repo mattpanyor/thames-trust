661152d1a6c1d863a9b10ea66d7d377288844eba
bweb85 <146823882+bweb85@users.noreply.github.com>
2024-03-19 20:51:46 +0000

    button changed

--- src/features/signUpForm/SignUpForm.jsx
+++ src/features/signUpForm/SignUpForm.jsx
@@ -128,7 +128,7 @@ export function SignUpForm() {
               Your password
             </label>
             <input
-              type="text"
+              type="password"
               onChange={handleChange}
               name="password"
               id="password"
@@ -136,7 +136,7 @@ export function SignUpForm() {
               className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
               required=""
             />
-            <button onClick={generatePassword}>click me </button>
+            <button onClick={generatePassword}>Generate Password </button>
             {/* <Button btnType={generatePassword} btnText={'Generate Password'} /> */}
           </div>
           <div className="mb-3 mt-6">
@@ -146,7 +146,7 @@ export function SignUpForm() {
               Confirm password
             </label>
             <input
-              type="text"
+              type="password"
               onChange={handleChange}
               name="confirmPassword"
               id="confirm-password"
