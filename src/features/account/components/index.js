ef49667fdba3c73f86f918d3db1e399ae6bbe7c0
JBraddockm <jbraddockm@gmail.com>
2024-03-19 04:49:10 +0000

    Exported new AccountPaymentForm component.

--- src/features/account/components/index.js
+++ src/features/account/components/index.js
@@ -1,3 +1,4 @@
 export { AccountCreateForm } from './AccountCreateForm.jsx';
 export { AccountList } from './AccountList.jsx';
 export { AccountListItem } from './AccountListItem.jsx';
+export { AccountPaymentForm } from './AccountPaymentForm.jsx';
