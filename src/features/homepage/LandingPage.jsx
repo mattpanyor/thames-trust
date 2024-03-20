02f15128c33531d8347701c8a79eff760ec01023
prangchira <prangchira@gmail.com>
2024-03-20 19:07:12 +0000

    add homepage

--- src/features/homepage/LandingPage.jsx
+++ src/features/homepage/LandingPage.jsx
@@ -8,7 +8,6 @@ export function LandingPage() {
     <>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
-          {AppLogo()}
           <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             Welcome to Thames Trust
           </h1>
@@ -17,6 +16,8 @@ export function LandingPage() {
             and pay bills with ease, anytime, anywhere.
           </p>
           <div className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
+            <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Let's Get Banking</h2>
+
             <button
               href="#"
               className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
@@ -34,7 +35,7 @@ export function LandingPage() {
             </button>
           </div>
           <div className="mx-auto px-4 text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
-            <span className="font-semibold uppercase text-gray-400">FEATURING</span>
+            <span className="font-semibold uppercase text-gray-400">POWERED BY</span>
             <div className="mt-8 flex flex-wrap items-center justify-center text-gray-500 sm:justify-between"></div>
           </div>
         </div>
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,9 +1,14 @@
 import { LandingPage } from 'src/features/homepage/LandingPage';
+import { NavbarFullWidthLayout } from 'src/layouts';
 
 export function Homepage() {
   return (
     <>
-      <LandingPage />
+      <NavbarFullWidthLayout>
+        <div className="pt:mt-0 mx-auto flex flex-col items-center justify-center px-6 pt-8 dark:bg-gray-900 md:h-screen">
+          <LandingPage />
+        </div>
+      </NavbarFullWidthLayout>
     </>
   );
 }
